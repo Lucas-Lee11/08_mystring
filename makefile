5519a178356b5d9ea298cfe28f6a362b03af9a51
Lucas Lee <lucasclee11@gmail.com>
2020-10-19 20:18:45 -0400

    mystring

---
+++ makefile
@@ -0,0 +1,8 @@
+all: main.o mystring.o
+	gcc -o string main.o mystring.o
+
+main.o: main.c mystring.h
+	gcc -c main.c
+
+euler.o: mystring.c mystring.h
+	gcc -c mystring.c
---
+++ mystring.c
@@ -0,0 +1,52 @@
+int mystrlen(char *str){
+    int i = 0;
+    while(*str){
+        i++;
+        str++;
+    }
+    return i;
+}
+
+char *mystrncpy(char *dst, char *src, int n){
+    int i = 0;
+    char *t = dst;
+    while(*src && i < n){
+        *dst = *src;
+        i++;
+        dst++; src++;
+    }
+    *dst = 0;
+    return t;
+}
+
+char *mystrcat(char *dst, char *src){
+    char *t = dst;
+    while(*dst) dst++;
+    while(*src){
+        *dst = *src;
+        dst++; src++;
+    }
+    *dst = 0;
+    return t;
+}
+
+int mystrcmp(char *str1, char *str2){
+    while(*str1 == *str2){
+        if(*str1 == 0 || *str2 == 0) return 0;
+        str1++; str2++;
+    }
+    return (*str1) - (*str2);
+}
+
+char * mystrchr(char *str, char c){
+    char *t = 0;
+    while(*str){
+        if (*str == c) {
+            t = str;
+            break;
+        }
+        str++;
+    }
+    if(c == 0) t = str;
+    return t;
+}
